02e2a880f8c8b6d8aa674157123839efcffbaec0
Pahuka <J-a-c-k@yandex.ru>
2021-08-23 13:57:34 +0500

    Реализовал задуманную структуру, формируется уже отсортированной по задаче. Осталось реализовать вывод данных в нудном формате.

--- DiskTreeTask.cs
+++ DiskTreeTask.cs
@@ -9,7 +9,7 @@ namespace DiskTree
     class Directory
     {
         private string dir;
-        public Directory SubDir;
+        public List<Directory> SubDir;
 
         public string Dir
         {
@@ -21,18 +21,30 @@ namespace DiskTree
 
         public Directory(List<string> input)
         {
-            if (input.Count > 0)
-            {
-                dir = input.First();
-                SubDir = new Directory(CalculateDirs(input, 1).Skip(1).ToList());
-            }
+            SubDir = new List<Directory>();
+            dir = input.First();
+
+            if (input.Count > 1)
+               SubDir.Add(new Directory(CalculateDirs(input, 1).Skip(1).ToList()));
+        }
+
+        public void SortDirs(List<Directory> newDir)
+        {
+            var tempDir = SubDir.Where(x => x.Dir == newDir.FirstOrDefault().Dir).FirstOrDefault();
+
+            if (tempDir != null)
+                tempDir.SortDirs(newDir.FirstOrDefault().SubDir);
+
+            else SubDir.Add(newDir.FirstOrDefault());
+            SubDir.Sort((a, b) => { return string.Compare(a.Dir, b.Dir, StringComparison.Ordinal); });
         }
 
         private List<string> CalculateDirs(List<string> input, int index)
         {
             for (int i = index; i < input.Count(); i++)
                 input[i] = input[i].Insert(0, " ");
-            if (index < input.Count()) CalculateDirs(input, index + 1);
+            if (index < input.Count())
+                CalculateDirs(input, index + 1);
 
             return input;
         }
@@ -42,45 +54,20 @@ namespace DiskTree
     {
         public static List<string> Solve(List<string> input)
         {
-            var list = new List<Directory>();
+            var list = new SortedDictionary<string, Directory>();
             var result = new List<string>();
 
             foreach (var dirs in input)
             {
-                list.Add(new Directory(dirs.Split('\\').ToList()));
-            }
-
-            list.Sort((a, b) => { return string.Compare(a.Dir, b.Dir, StringComparison.Ordinal); });
-
-
-            foreach (var dirs in list)
-            {
-                if (dirs.SubDir.Dir == null) result.Add(dirs.Dir);
-                else
+                var temp = new Directory(dirs.Split('\\').ToList());
+                if (list.ContainsKey(temp.Dir))
                 {
-                    if (!result.Contains(dirs.Dir)) result.Add(dirs.Dir);
-                    result.AddRange(Sort(list.Where(x => x.Dir == dirs.Dir && x.SubDir.Dir != null).ToList()));
+                    list[temp.Dir].SortDirs(temp.SubDir);
                 }
+                else list.Add(temp.Dir, temp);
             }
 
             return null;
-        }
-
-        public static List<string> Sort(List<Directory> list)
-        {
-            var result = new List<string>();
-
-            foreach (var item in list.OrderBy(x => x.SubDir.Dir, StringComparer.Ordinal))
-            {
-                var stepDir = item.SubDir;
-                while (stepDir.Dir != null)
-                {
-                    result.Add(stepDir.Dir);
-                    stepDir = stepDir.SubDir;
-                }
-            }
-
-            return result;
         }
     }
 }
